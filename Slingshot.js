59e8e3ef621ef38e781364e11d9de01324d1b25c
sristi <send2sristi@gmail.com>
2020-06-21 16:10:47 +0530

    angry birds with sling

--- Slingshot.js
+++ Slingshot.js
@@ -8,7 +8,11 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+       this.slingshot1 = loadImage("sprites/sling1.png");
+       this.slingshot2 = loadImage("sprites/sling2.png");
+       this.slingshot3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
+
     }
 
     fly(){
@@ -16,11 +20,26 @@ class SlingShot{
     }
 
     display(){
+        image(this.slingshot1,170,20);
+        image(this.slingshot2,140,20);
+        
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+           
+            stroke(48,22,8);
+            if (pointA.x<220){
+                strokeWeight(10);
+                line(pointA.x-20, pointA.y, pointB.x, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x-50, pointB.y);
+                image(this.slingshot3,pointA.x-30,pointA.y-10,15,30);   
+            }else{
+                strokeWeight(6);
+            line(pointA.x+25, pointA.y, pointB.x, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x-50, pointB.y);
+            image(this.slingshot3,pointA.x+25,pointA.y-10,15,30);
+            }
         }
     }
     
